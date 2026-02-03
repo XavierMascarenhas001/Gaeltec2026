c298e70127f626e52f449633dc123e54d468f68c
XavierMascarenhas001 <xavier.mascarenhas@gaeltecutilities.com>
2026-02-03 13:45:07 +0000

    Update Gaeltec2026.py

--- Gaeltec2026.py
+++ Gaeltec2026.py
@@ -1132,7 +1132,7 @@ if misc_file is not None:
 
             # Select only columns that exist in df
             cols_to_include = [
-                'Output', 'Quantity', 'material_code', 'pole', 'Date',
+                'Output','item', 'Quantity_original','Quantity_used', 'material_code', 'pole', 'Date',
                 'District', 'project', 'Project Manager', 'Circuit', 'Segment',
                 'team lider', 'PID', 'sourcefile'
             ]
